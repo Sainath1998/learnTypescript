bd314e468239c6551f4716f8e9603384664d2d9c
SainathKamble <saianthkamble263@gmail.com>
2022-07-09 20:33:46 +0530

    commit

--- types.ts
+++ types.ts
@@ -11,3 +11,13 @@ type lockstates = "locled" | "unlocked"
 
 
 //  lockstates type now lockled unlocked
+
+
+
+// get:ength takes string or array os string and return the length of it 
+function getLength (obj : string | string []){
+    return obj.length
+}
+
+console.log(getLength(["Sainath","is","new","to","typescript"])) // logs 5 in console
+console.log(getLength("Sainath is new to typescript")) // logs 28 in console
