36d793e48ff21139a9adaf80e4447589fe5b0ec2
SainathKamble <saianthkamble263@gmail.com>
2022-07-09 22:04:59 +0530

    commit

---
+++ encapsulation.ts
@@ -0,0 +1,40 @@
+// encapsulation is used to hide the data and methods that are meant to be internal and only required for inner working of the object.
+
+class Player {
+    private health!: number;
+    private stamina!: number;
+
+     setHealth(newHeatlh : number){
+        if(newHeatlh < 0 ){
+            console.log('Sorry health cannot be less than 0');
+        }else{
+            this.health = newHeatlh
+            
+        }
+    }
+
+     getHealth(){
+        return this.health
+    }
+
+     setStamina(newStamina : number){
+        if(newStamina < 0 ){
+            console.log('The stamina cannot be less than 0');
+        }else{
+            this.stamina = newStamina
+            
+        }
+    }
+
+     getStamina (){
+        return this.stamina
+    }
+}
+
+const Sainath = new Player();
+
+Sainath.setHealth(10);
+Sainath.setStamina(10);
+
+
+console.log(Sainath.getHealth());
